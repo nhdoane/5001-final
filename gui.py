7b20a215f39052dd7e135742b667c2dd011d4b55
Nick Doane <nhdoane@proton.me>
2024-12-02 14:59:39 -0500

    Add search functionality
    -added search functionality to GUI
      -date
      -name
      -bill ID

--- gui.py
+++ gui.py
@@ -6,7 +6,9 @@ import os.path
 import sys
 import tkinter as tk
 from tkinter import ttk, messagebox, font, filedialog
+
 # custom methods/classes
+from conversions.conversions import convert_from_storage
 from input import bill_entry, bill_list, remove_bill, reset_database, backup_database
 from search.search import Search
 
@@ -40,7 +42,7 @@ class GUI(tk.Tk):
         Main menu displayed in the left_frame
         """
         ttk.Button(self.left_frame, text='Add Bill', command=self._bill_add).pack(side='top')
-        ttk.Button(self.left_frame, text='List Bills', command=self._bill_list_table).pack(side='top')
+        ttk.Button(self.left_frame, text='List Bills', command=self._bill_list).pack(side='top')
         ttk.Button(self.left_frame, text='Search Bills', command=self._search).pack(side='top')
         ttk.Button(self.left_frame, text='Remove Bill', command=self._remove_bill).pack(side='top')
         ttk.Button(self.left_frame, text='Quit', command=self.__close).pack(side='bottom')
@@ -51,7 +53,7 @@ class GUI(tk.Tk):
         """
         Class helper method for adding a new bill
         """
-        self.__clear_frame()
+        self.__clear_frame(self.right_frame)
         bill_name = tk.StringVar()
         bill_desc = tk.StringVar()
         bill_amt = tk.StringVar()
@@ -66,59 +68,105 @@ class GUI(tk.Tk):
         ttk.Label(self.right_frame, text='Due Date:').pack()
         ttk.Entry(self.right_frame, textvariable=bill_dd).pack()
 
-    def _bill_list_table(self):
+    def _bill_list(self):
         """
         Class helper method for fetching and displaying the bill data
         """
-        self.__clear_frame()
+        self.__clear_frame(self.right_frame)
         bills = bill_list()
-        columns = ('bill_id', 'name', 'amt', 'due_date', 'desc')
-        table = ttk.Treeview(self.right_frame, columns=columns, show='headings')
-        table.column('bill_id', width=75)
-        table.column('name',width=150)
-        table.column('amt', width=75)
-        table.column('due_date', width=100)
-        table.column('desc', width=275)
-        table.heading('bill_id', text='Bill ID')
-        table.heading('name', text='Name')
-        table.heading('amt', text='Amount')
-        table.heading('due_date', text='Due Date')
-        table.heading('desc', text='Description')
-        for bill in bills:
-            table.insert("", tk.END, values=(bill[0], bill[1], bill[2], bill[3], bill[4]))
-        vsb = ttk.Scrollbar(self.right_frame, orient='vertical', command=table.yview)
-        vsb.pack(side='right', fill='y')
-        table.pack(side='right', fill='both')
-        table.configure(yscrollcommand=vsb.set)
+        self.__bill_table(bills, self.right_frame)
 
     def _search(self):
         """
         Class helper method containing the logic for displaying and choosing bill search functionality
         """
-        self.__clear_frame()
-        ttk.Label(self.right_frame, text='Search by:').grid(column=1,row=0, sticky='W')
-        ttk.Button(self.right_frame, text='Date').grid(column=1, row=1, sticky='W')
-        ttk.Button(self.right_frame, text='Name').grid(column=1, row=2, sticky='W')
-        ttk.Button(self.right_frame, text='Bill ID').grid(column=1, row=3, sticky='W')
+        self.__clear_frame(self.right_frame)
+        self.search_options = ttk.Frame(self.right_frame)
+        self.search_results = ttk.Frame(self.right_frame)
+        self.search_options.pack(side='top', fill='both')
+        self.search_results.pack(side='bottom', fill='both')
+        ttk.Label(self.search_options, text='Search by:').pack(side='top', anchor='nw')
+        ttk.Button(self.search_options, text='Date', command=self._search_date).pack(side='left', anchor='nw')
+        ttk.Button(self.search_options, text='Name', command=self._search_name).pack(side='left', anchor='nw')
+        ttk.Button(self.search_options, text='Bill ID', command=self._search_billid).pack(side='left', anchor='nw')
+
 
     # TODO: search GUI needs to be completed
     def _search_date(self):
         """
         Class helper method for displaying search-by-date
         """
-        pass
+        if len(self.search_options.winfo_children()) > 4:
+            self.__search_clear()
+        date = tk.StringVar()
+        ttk.Button(self.search_options, text='Search', command=lambda: self.__sd_helper(date.get())).pack(side='right',
+                                                                                                          anchor='ne')
+        ttk.Entry(self.search_options, textvariable=date).pack(side='right', anchor='ne', padx=5, pady=2)
+        ttk.Label(self.search_options, text='Enter date (e.g. 2024-04-14)').pack(side='right', anchor='ne', pady=2)
+
+    def __sd_helper(self, date):
+        self.__clear_frame(self.search_results)
+        query = Search()
+        query.set_query(date)
+        resp = query.search_date()
+        bills = []
+        for bill in resp:
+            amount = format(convert_from_storage(bill[4]), '.2f')
+            bills.append([bill[0], bill[2], amount, bill[5], bill[3]])
+        self.__bill_table(bills, self.search_results)
 
     def _search_name(self):
         """
         Class helper method for displaying search-by-name
         """
-        pass
+        if len(self.search_options.winfo_children()) > 4:
+            self.__search_clear()
+        name = tk.StringVar()
+        ttk.Button(self.search_options, text='Search', command=lambda: self.__sn_helper(name.get())).pack(side='right',
+                                                                                                          anchor='ne')
+        ttk.Entry(self.search_options, textvariable=name).pack(side='right', anchor='ne', padx=5, pady=2)
+        ttk.Label(self.search_options, text='Enter the bill name').pack(side='right', anchor='ne', pady=2)
+
+    def __sn_helper(self, name):
+        self.__clear_frame(self.search_results)
+        query = Search()
+        query.set_query(name)
+        resp = query.search_name()
+        bills = []
+        for bill in resp:
+            amount = format(convert_from_storage(bill[4]), '.2f')
+            bills.append([bill[0], bill[2], amount, bill[5], bill[3]])
+        self.__bill_table(bills, self.search_results)
 
     def _search_billid(self):
         """
-        Class helper method for displaying serach-by-bill ID
+        Class helper method for displaying search-by-bill ID
         """
-        pass
+        if len(self.search_options.winfo_children()) > 4:
+            self.__search_clear()
+        billid = tk.StringVar()
+        ttk.Button(self.search_options, text='Search', command=lambda: self.__sid_helper(billid.get())).pack(side='right',
+                                                                                                          anchor='ne')
+        ttk.Entry(self.search_options, textvariable=billid).pack(side='right', anchor='ne', padx=5, pady=2)
+        ttk.Label(self.search_options, text='Enter the bill ID').pack(side='right', anchor='ne', pady=2)
+
+    def __sid_helper(self, billid):
+        self.__clear_frame(self.search_results)
+        query = Search()
+        query.set_query(int(billid))
+        resp = query.search_billID()
+        print(resp)
+        bills = []
+        amount = format(convert_from_storage(resp[0][4]), '.2f')
+        bills.append([resp[0][0], resp[0][2], amount, resp[0][5], resp[0][3]])
+        self.__bill_table(bills, self.search_results)
+
+    def __search_clear(self):
+        """
+        Clears out the last three added items to the search_options frame, which will always be the specific search entry/labels
+        """
+        for i in range(3):
+            self.search_options.winfo_children()[-1].destroy()
 
     def _remove_bill(self):
         """
@@ -130,7 +178,7 @@ class GUI(tk.Tk):
         """
         Class helper method for displaying the settings/options page
         """
-        self.__clear_frame()
+        self.__clear_frame(self.right_frame)
         default_font = get_font_name()
         settings_header = ttk.Label(self.right_frame, text='Implementation Options')
         settings_header.config(font=font.Font(family=default_font['family'], size=11, underline=True, weight='bold'))
@@ -190,13 +238,38 @@ class GUI(tk.Tk):
         if messagebox.askquestion('Quit', 'Are you sure you want to quit?') == 'yes':
             sys.exit(0)
 
-    def __clear_frame(self):
+    def __clear_frame(self, frame):
         """
         'Private' method used for clearing the right frame when selecting different options
         """
-        for widget in self.right_frame.winfo_children():
+        for widget in frame.winfo_children():
             widget.destroy()
-        self.right_frame.grid_forget()
+        frame.grid_forget()
+
+    def __bill_table(self, bills, frame):
+        if not bills:
+            no_results = ttk.Label(self.search_results, text='The search returned no results')
+            no_results.config(font=font.Font(family=get_font_name()['family'], size=12, weight='bold'))
+            no_results.pack(side='top', anchor='center')
+        else:
+            columns = ('bill_id', 'name', 'amt', 'due_date', 'desc')
+            table = ttk.Treeview(frame, columns=columns, show='headings')
+            table.column('bill_id', width=75)
+            table.column('name', width=150)
+            table.column('amt', width=75)
+            table.column('due_date', width=100)
+            table.column('desc', width=275)
+            table.heading('bill_id', text='Bill ID')
+            table.heading('name', text='Name')
+            table.heading('amt', text='Amount')
+            table.heading('due_date', text='Due Date')
+            table.heading('desc', text='Description')
+            for bill in bills:
+                table.insert("", tk.END, values=(bill[0], bill[1], bill[2], bill[3], bill[4]))
+            vsb = ttk.Scrollbar(frame, orient='vertical', command=table.yview)
+            vsb.pack(side='right', fill='y')
+            table.pack(side='right', fill='both', expand=True)
+            table.configure(yscrollcommand=vsb.set)
 
 
 if __name__ == '__main__':
